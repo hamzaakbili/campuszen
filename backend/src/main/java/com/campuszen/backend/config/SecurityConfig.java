2e40568e395f2da2a7d0300d3d83a62f4ca90984
hamzaakbili <hamzaakbili386@gmail.com>
2026-02-24 15:44:05 +0100

    Update SecurityConfig.java

--- backend/src/main/java/com/campuszen/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/campuszen/backend/config/SecurityConfig.java
@@ -41,7 +41,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+    "http://localhost:4200",
+    "https://campuszen-app.vercel.app"
+));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
