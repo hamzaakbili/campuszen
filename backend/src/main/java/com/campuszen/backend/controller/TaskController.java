cdcca9ee7f11ff571f7a9653188acd07e2d03bb0
hamzaakbili <hamzaakbili386@gmail.com>
2026-02-24 15:19:20 +0100

    Update TaskController.java

--- backend/src/main/java/com/campuszen/backend/controller/TaskController.java
+++ backend/src/main/java/com/campuszen/backend/controller/TaskController.java
@@ -11,7 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {
+  "http://localhost:4200",
+  "https://campuszen-app.vercel.app"
+})
 public class TaskController {
 
     @Autowired
