baef3ec1b0533ad6ee12746309a5e1dd54d93826
hamzaakbili <hamzaakbili386@gmail.com>
2026-02-24 15:17:00 +0100

    Update AuthController.java

--- backend/src/main/java/com/campuszen/backend/controller/AuthController.java
+++ backend/src/main/java/com/campuszen/backend/controller/AuthController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {
+  "http://localhost:4200",
+  "https://campuszen-app.vercel.app"
+})
 public class AuthController {
 
     @Autowired
